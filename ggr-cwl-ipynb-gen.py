061ba3d113fa3941f8f38e024d2452344ab1da9c
Alejandro Barrera <alejandro.barrera@duke.edu>
2017-10-23 17:40:40 -0400

    Darrylyan46 master (#3)
    This wraps PR #2 and a few more improvements
    
    * Added QC and fingerprint plot cells
    
    * Bug fixes and changed hardcoded paths
    
    * Bug fixes
    
    * Pull request bug fixes
    
    * Merge conflict
    
    * Second pull request
    
    * Bug fixes with CellSbatch
    
    * Changes to PR #2
    
    - Change constant for conda activate script
    - Change plotFingerprint sbatch command to avoid wrapping
    - Change QC Cell to CellSbatch, reorganize internal code

--- ggr-cwl-ipynb-gen.py
+++ ggr-cwl-ipynb-gen.py
@@ -282,19 +282,18 @@ def generate_qc_cell(conf_args, lib_type, pipeline_type):
     elif end_type == "pe":
         end_type = "paired_end"
 
-    logs_dir = "%s/processing/%s/logs" % (conf_args['root_dir'], lib_type)
-    execute_cell = CellSbatch(contents=["cd %s/processing/%s/%s-%s" % (conf_args['root_dir'],
-                                                                       lib_type, conf_args['project_name'],
-                                                                       pipeline_type),
-                                        "python %s/generate_stats_%s_%s.py" % (consts.qc_script_dir,
-                                                                               lib_type.replace("_", ""),
-                                                                               end_type),
-                                        "-samples", "`/bin/ls -1 *PBC.txt | sed 's@.PBC.txt@@'` > qc.txt"],
-                              depends_on=True,
-                              wrap_command='',
-                              description="#### Generate QCs for %s %s" % (lib_type, pipeline_type),
-                              script_output="%s/%s-%s_generate_qc.out" % (logs_dir, conf_args['project_name'],
-                                                                          pipeline_type))
+    execute_cell = CellSbatch(
+        prolog=["source %s alex" % consts.conda_activate],
+        contents=list(),
+        wrap_command=" \\\n\t".join([
+            "cd %s/processing/%s/%s-%s;" % (conf_args['root_dir'],
+                                         lib_type,
+                                         conf_args['project_name'],
+                                         pipeline_type),
+            "python %s/generate_stats_%s_%s.py ./ -samples $(/bin/ls -1 *PBC.txt | sed 's@.PBC.txt@@')" % (consts.qc_script_dir, lib_type.replace("_", ""), end_type),
+            "> qc.txt"]),
+        description="### Generate QCs for %s %s" % (lib_type, pipeline_type)
+                        )
     cells.extend(execute_cell.to_list())
 
     return cells
@@ -323,13 +322,17 @@ def generate_plots(conf_args, metadata_file, lib_type, pipeline_type):
                                         "%s/fingerprint_and_spp/%s-%s" % (conf_args['root_dir'],
                                                                           conf_args['project_name'],
                                                                           pipeline_type)],
+                              wrap=False,
                               depends_on=True,
-                              wrap_command='mkdir -p %s/fingerprint_and_spp/%s-%s %s/fingerprint_and_spp/logs' %
-                                           (conf_args['root_dir'], conf_args['project_name'],
-                                            pipeline_type, conf_args['root_dir']),
+                              prolog=['mkdir -p \\\n\t%s/fingerprint_and_spp/%s-%s \\\n\t'
+                                      '%s/fingerprint_and_spp/logs' % (conf_args['root_dir'],
+                                                                       conf_args['project_name'],
+                                                                       pipeline_type,
+                                                                       conf_args['root_dir'])],
                               script_output="%s/fingerprint_and_spp/logs" % conf_args['root_dir'],
                               description="#### Generate fingerprint plots for %s-%s" % (conf_args['project_name'],
-                                                                                         pipeline_type))
+                                                                                         pipeline_type),
+                              partition="new,all")
     cells.extend(execute_cell.to_list())
 
     return cells
@@ -403,7 +406,7 @@ def create_cells(samples_df, conf_args=None):
             cells.extend(cwl_slurm_array_gen(conf_args, lib_type, metadata_filename=metadata_file,
                                              pipeline_type=pipeline_type, n_samples=n))
             cells.extend(generate_qc_cell(conf_args, lib_type, pipeline_type=pipeline_type))
-	    cells.extend(generate_plots(conf_args, metadata_file=metadata_file,
+            cells.extend(generate_plots(conf_args, metadata_file=metadata_file,
                                         lib_type=lib_type, pipeline_type=pipeline_type))
     return cells
 
